72a91f212a55c439f8e9b1bf426e526bf8f668bc
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-28 20:07:48 +0100

    add pack prototype and unit test

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ pub struct TSPackAttributes {
     pub microseconds_time_window: u64,
 }
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq)]
 pub enum TSPackError {
     #[error("microseconds_time_window must be > 0")]
     InvalidWindow,
@@ -45,6 +45,21 @@ impl TimeSeriesDataPacker {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn pack(
+        &mut self,
+        mut samples: Vec<TSSamples>,
+        attributes: TSPackAttributes,
+    ) -> Result<Vec<TSPackedSamples>, TSPackError> {
+        if attributes.microseconds_time_window == 0 {
+            return Err(TSPackError::InvalidWindow);
+        }
+
+        todo!();
+
+        Ok(vec!())
+    }
+
 }
 
 #[cfg(test)]
@@ -69,9 +84,26 @@ mod tests {
         };
 
         let packed = packer.pack(samples.clone(), attrs).unwrap();
+
         assert_eq!(packed.len(), 3);
         assert_eq!(packed[0], ((0.0, 0.2), 100.0));
         assert_eq!(packed[1], ((0.3, 0.4), 101.0));
         assert_eq!(packed[2], ((0.5, 0.5), 100.0));
+    }
+
+    #[test]
+    fn test_invalid_window_error_in_data_packer_pack() {
+        let samples = vec![
+        ];
+
+        let mut packer = TimeSeriesDataPacker::new();
+        let attrs = TSPackAttributes {
+            strategy_types: vec![TSPackStrategyType::TSPackSimilarValuesStrategy],
+            microseconds_time_window: 0
+        };
+
+        let result = packer.pack(samples.clone(), attrs);
+
+        assert_eq!(result, Err(TSPackError::InvalidWindow));
     }
 }
